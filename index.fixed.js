b00af776f2230aa9655c07742a8828b91e605f0c
BugHunter-X <askzenai@gmail.com>
2024-12-05 13:22:05 +0100

    Add index.fixed.js file

---
+++ index.fixed.js
@@ -0,0 +1,11 @@
+```javascript
+// pages/index.fixed.js
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <p>This is a test.</p>
+    </div>
+  );
+}
+```
